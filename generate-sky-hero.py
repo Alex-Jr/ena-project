063b0f83911e91058b23814aaa35549118df987d
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2021-11-03 21:31:02 -0300

    rankNumber e points

--- generate-sky-hero.py
+++ generate-sky-hero.py
@@ -9,8 +9,7 @@ players_ids = []
 with open('players-ids.txt', 'r') as file:
     players_ids = file.readlines()
 
-names = []
-medals = []
+player_data = []
 base_url = 'https://www.erepublik.com/en/main/citizen-profile-json/'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
@@ -29,34 +28,42 @@ headers = {
 for player_id in players_ids:
     url = base_url + player_id.replace('\n', '')
     response = requests.get(url=url, headers=headers)
-
-    medal_count = -1
-    name = 'error'
+   
     if(response.status_code == 200):   
         parsed_response = json.loads(response.content)
-        medal_count = parsed_response['achievements'][6]['count']
-        name = parsed_response['citizen']["name"]
-
-    names.append(name)
-    medals.append(medal_count)
+        player_data.append({
+            'id': player_id,
+            'name': parsed_response['citizen']["name"],
+            'sky_hero_medals': parsed_response['achievements'][6]['count'],
+            'air_rank_number': parsed_response['military']['militaryData']['aircraft']['rankNumber'],
+            'air_rank_title':  parsed_response['military']['militaryData']['aircraft']['name'],
+            'air_rank_points': parsed_response['military']['militaryData']['aircraft']['points'],
+        })
+    else:
+        player_data.append({
+            'id': player_id,
+            'name': 'error',
+            'sky_hero_medals': '0',
+            'air_rank_number': '0',
+            'air_rank_title': 'error',
+            'air_rank_points': '0'
+        })
+    
     timeout = 20 + random.random() * 10
-    print('Fetched {} - Sleeping for {:1f} seconds'.format(name, timeout))
+    print('Fetched id: {} - Sleeping for {:1f} seconds'.format(player_id, timeout))
     time.sleep(timeout)
 
-# offline tests
-# content = []
-# with open('mockup.txt', 'r') as file:
-#     content = file.readlines()
-# parsed_response = json.loads(content[0])
-# medals.append(parsed_response['achievements'][6]['count'])
-# names.append(parsed_response['citizen']["name"])
-
 with open('generate-sky-hero-output.csv', 'w') as file:
-    headers= ['name', 'sky_hero_medals']
+    headers= [
+        'id',
+        'name',
+        'sky_hero_medals',
+        'air_rank_number',
+        'air_rank_title',
+        'air_rank_points'
+    ]
     writer = csv.DictWriter(file, fieldnames=headers)
+    writer.writeheader()
 
-    i = 0
-    for name in names:
-        writer.writerow({'name': name, 'sky_hero_medals': medals[i]})
-        i = i + 1
-
+    for data in player_data:
+        writer.writerow(data)
